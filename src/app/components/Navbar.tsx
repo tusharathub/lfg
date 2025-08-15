a20571105ce7ff177cbfc401216235ef22df89cf
Tushar <tushar.nyc20@gmail.com>
2025-08-15 13:51:07 +0530

    created navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-50 shadow-sm">
+      <div className="container mx-auto px-4 py-3 flex items-center justify-between">
+        <h1 className="text-xl font-bold">GameMate</h1>
+        <div className="space-x-4">
+          <a href="/" className="hover:underline"> Home </a>
+          <a href="/create" className="hover:underline"> Create Session</a>
+          <a href="" className="hover:underline"> Profile</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className="bg-gray-100 text-gray-900">
+        <Navbar/>
+        <main className="container mx-auto px-4 py-6"> {children} </main>
       </body>
     </html>
   );
